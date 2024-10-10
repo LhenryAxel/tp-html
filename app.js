64d3e12781e377f57c45db99bcfa093e2545fdc9
axelnolan.lhenry@gmail.com <axelnolan.lhenry@gmail.com>
2024-10-10 11:28:19 +0200

    Added js button

---
+++ app.js
@@ -0,0 +1,64 @@
+const showMoreLeBron = document.getElementById('showMoreLeBron');
+const moreInfoLeBron = document.getElementById('moreInfoLeBron');
+
+showMoreLeBron.addEventListener('click', function() {
+    if (moreInfoLeBron.style.display === 'none' || moreInfoLeBron.style.display === '') {
+        moreInfoLeBron.style.display = 'block'; 
+        showMoreLeBron.textContent = 'Voir moins'; 
+    } else {
+        moreInfoLeBron.style.display = 'none'; 
+        showMoreLeBron.textContent = 'Voir plus'; 
+    }
+});
+
+const showMoreCurry = document.getElementById('showMoreCurry');
+const moreInfoCurry = document.getElementById('moreInfoCurry');
+
+showMoreCurry.addEventListener('click', function() {
+    if (moreInfoCurry.style.display === 'none' || moreInfoCurry.style.display === '') {
+        moreInfoCurry.style.display = 'block'; 
+        showMoreCurry.textContent = 'Voir moins'; 
+    } else {
+        moreInfoCurry.style.display = 'none'; 
+        showMoreCurry.textContent = 'Voir plus'; 
+    }
+});
+
+const showMoreDurant = document.getElementById('showMoreDurant');
+const moreInfoDurant = document.getElementById('moreInfoDurant');
+
+showMoreDurant.addEventListener('click', function() {
+    if (moreInfoDurant.style.display === 'none' || moreInfoDurant.style.display === '') {
+        moreInfoDurant.style.display = 'block'; 
+        showMoreDurant.textContent = 'Voir moins'; 
+    } else {
+        moreInfoDurant.style.display = 'none'; 
+        showMoreDurant.textContent = 'Voir plus'; 
+    }
+});
+
+const showMoreIrving = document.getElementById('showMoreIrving');
+const moreInfoIrving = document.getElementById('moreInfoIrving');
+
+showMoreIrving.addEventListener('click', function() {
+    if (moreInfoIrving.style.display === 'none' || moreInfoIrving.style.display === '') {
+        moreInfoIrving.style.display = 'block'; 
+        showMoreIrving.textContent = 'Voir moins'; 
+    } else {
+        moreInfoIrving.style.display = 'none'; 
+        showMoreIrving.textContent = 'Voir plus'; 
+    }
+});
+
+const showMoreDoncic = document.getElementById('showMoreDoncic');
+const moreInfoDoncic = document.getElementById('moreInfoDoncic');
+
+showMoreDoncic.addEventListener('click', function() {
+    if (moreInfoDoncic.style.display === 'none' || moreInfoDoncic.style.display === '') {
+        moreInfoDoncic.style.display = 'block'; 
+        showMoreDoncic.textContent = 'Voir moins'; 
+    } else {
+        moreInfoDoncic.style.display = 'none'; 
+        showMoreDoncic.textContent = 'Voir plus'; 
+    }
+});
